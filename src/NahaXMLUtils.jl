a93e8a1ba5ecacb949ddcac0b8e73aeac17ad895
MarkNahabedian <naha@mit.edu>
2025-08-19 08:17:02 -0400

    Searched my julia packages for other XML namespace URIs.  Just found SHAPER_NAMESPACE.  Adding it.

--- src/NahaXMLUtils.jl
+++ src/NahaXMLUtils.jl
@@ -6,9 +6,11 @@ using OrderedCollections
 using Unitful
 using UnitfulUS
 
-export Bounds, bounds_to_viewbox, elt
+export SVG_NAMESPACE, Bounds, bounds_to_viewbox, elt
 
+# XML namespaces I care about:
 const SVG_NAMESPACE = "http://www.w3.org/2000/svg"
+SHAPER_NAMESPACE = "http://www.shapertools.com/namespaces/shaper"
 
 Base.@kwdef struct Bounds
     minX::Real
